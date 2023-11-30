8922496b3d7ec70f60268f13ecafa2c7b910c3f6
Andersonmelodamasceno <dinho.live01@gmail.com>
2023-11-30 17:12:46 -0300

    First

---
+++ FilmesApi/Program.cs
@@ -0,0 +1,32 @@
+using FilmesApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<FilmeContext>(options =>
+{
+    options.UseSqlServer("Server=tcp:bancomssqlserver.database.windows.net,1433;Initial Catalog=bancomssqlserver;Persist Security Info=False;User ID=useradmin;Password=@Abc1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
